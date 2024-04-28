13e627f316db9634aedc0d6b44ef69f7fcd66e6e
Kai Mikkelsen <43660096+KaiMikkelsen@users.noreply.github.com>
2024-04-28 13:26:07 -0400

    Update music_demixing.py

--- evaluator/music_demixing.py
+++ evaluator/music_demixing.py
@@ -35,7 +35,7 @@ def time_limit(seconds):
 class MusicDemixingPredictor:
     def __init__(self):
         self.test_data_path = os.getenv("TEST_DATASET_PATH", "/content/drive/MyDrive/dataset2")
-        self.results_data_path = os.getenv("RESULTS_DATASET_PATH", os.getcwd() + "/data/results/")
+        self.results_data_path = os.getenv("RESULTS_DATASET_PATH", os.getcwd())
         self.inference_setup_timeout = int(os.getenv("INFERENCE_SETUP_TIMEOUT_SECONDS", "900"))
         self.inference_per_music_timeout = int(os.getenv("INFERENCE_PER_MUSIC_TIMEOUT_SECONDS", "240"))
         self.partial_run = os.getenv("PARTIAL_RUN_MUSIC_NAMES", None)
